74449e20547620f97c97a863c00164d503fb23df
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-07-04 09:13:56 +0530

    Added solution - LeetHub

---
+++ Minimum X (xor) A - GFG/minimum-x-xor-a.cpp
@@ -0,0 +1,93 @@
+// { Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int count_set_bit(int n){
+      int ans = 0;
+      
+      while(n){
+          if(n%2 == 1){
+              ans++;
+          }
+          n = n/2;
+      }
+      return ans;
+  }
+    int minVal(int a, int b) {
+        // code here
+        vector<int> a_bits(32,0);
+        int on = 0;
+        int i=0;
+        while(a){
+            a_bits[i] = a%2;
+            i++;
+            if(a%2 == 1){
+                on++;
+            }
+            a = a/2;
+        }
+        // 11
+        int cnt = count_set_bit(b);
+        //cout << on << " " << cnt;
+        int res = 0;
+        if(cnt > on){
+            cnt = cnt-on;
+          
+            for(int i=0;i<a_bits.size();i++){
+                
+                if(cnt == 0){
+                    break;
+                }
+                if(a_bits[i] == 0){
+                
+                    a_bits[i] = 1;
+                    cnt--;
+                }
+            }
+            
+        }else{
+            cnt = on-cnt;
+            for(int i=0;i<a_bits.size();i++){
+                if(cnt == 0){
+                    break;
+                }
+                if(a_bits[i] == 1){
+                    a_bits[i] = 0;
+                    cnt--;
+                }
+            }
+        }
+           int p=0;
+            for(int i=0;i<a_bits.size();i++){
+             //   cout << a_bits[i] << " ";
+                res += a_bits[i]*pow(2,p);
+                p++;
+            }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+
+        Solution ob;
+        cout << ob.minVal(a, b);
+
+        cout << "\n";
+    }
+
+    return 0;
+}  // } Driver Code Ends
