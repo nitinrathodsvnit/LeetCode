05d61710ceb88b53bb816b5bc3c14aed3305b857
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-07-10 07:22:33 +0530

    Time: 11 ms (53.51%), Space: 13.7 MB (51.95%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -14,12 +14,14 @@ public:
     // }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n + 1);  // dp[i] is minimum cost to reach to i_th floor
+        int prev = 0, sec_prev = 0; 
         for (int i = 2; i <= n; i++) {
-            int jumpOneStep = dp[i - 1] + cost[i - 1];  // Minimum cost if we jump 1 step from floor (i-1)_th to i_th floor
-            int jumpTwoStep = dp[i - 2] + cost[i - 2];  // Minimum cost if we jump 2 steps from floor (i-2)_th to i_th floor
-            dp[i] = min(jumpOneStep, jumpTwoStep);
+            int jumpOneStep = prev + cost[i - 1];  
+            int jumpTwoStep = sec_prev + cost[i - 2]; 
+            sec_prev = prev;
+            prev = min(jumpOneStep, jumpTwoStep);
+            
         }
-        return dp[n];
+        return prev;
     }
 };
