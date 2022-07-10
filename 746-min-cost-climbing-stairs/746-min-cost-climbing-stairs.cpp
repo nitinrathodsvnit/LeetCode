a9e2c514e09ca9d208cc8519d5fb92a784f6a272
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-07-10 07:08:34 +0530

    Time: 18 ms (15.56%), Space: 13.7 MB (79.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int f(vector<int> a, int i, vector<int>&dp){
-        if(i<=1)    return dp[i] = a[i];
-        if(dp[i]!=-1)   return dp[i];
-        int pick =  f(a, i-1, dp);
-        int nonpick = f(a, i-2, dp);
-        return dp[i] = a[i] + min(pick, nonpick);
-    }
+    // int f(vector<int> a, int i, vector<int>&dp){
+    //     if(i<=1)    return dp[i] = a[i];
+    //     if(dp[i]!=-1)   return dp[i];
+    //     int pick =  f(a, i-1, dp);
+    //     int nonpick = f(a, i-2, dp);
+    //     return dp[i] = a[i] + min(pick, nonpick);
+    // }
+    // int minCostClimbingStairs(vector<int>& cost) {
+    //     int n = cost.size();
+    //     vector<int> dp(n+1, -1);
+    //     return min(f(cost, n-1, dp), f(cost, n-2, dp));
+    // }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int> dp(n+1, -1);
-        return min(f(cost, n-1, dp), f(cost, n-2, dp));
+        vector<int> dp(n + 1);  // dp[i] is minimum cost to reach to i_th floor
+        for (int i = 2; i <= n; i++) {
+            int jumpOneStep = dp[i - 1] + cost[i - 1];  // Minimum cost if we jump 1 step from floor (i-1)_th to i_th floor
+            int jumpTwoStep = dp[i - 2] + cost[i - 2];  // Minimum cost if we jump 2 steps from floor (i-2)_th to i_th floor
+            dp[i] = min(jumpOneStep, jumpTwoStep);
+        }
+        return dp[n];
     }
 };
