0d502cae8c96b5fc3d2f5ba81cc381884f7fb330
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-07-21 16:49:57 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (90.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+    void sortColors(vector<int>& a) {
+        int n = a.size();
+        int low =0 , high = n-1, mid = 0;
+        while(mid<=high){
+            if(a[mid]==0){
+                swap(a[low++], a[mid++]);
+            }
+            else if(a[mid]==1) mid++;
+            else if(a[mid]==2){
+                swap(a[mid], a[high--]);
+            }
+        }
     }
 };
