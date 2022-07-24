13e1ce993c197246f7b9d8e8418ac66a245e4b85
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-07-25 00:07:54 +0530

    Time: 13 ms (29.26%), Space: 6 MB (24.83%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long long x=1, i=1;
-        while(x<n){
-            i++;
-            x+=i;
-        }
-        // cout<<x<<" "<<n;
-        if(x==n)    return i;
-        return i-1;
+        return floor(-0.5+sqrt((double)2*n+0.25));
+        // long long x=1, i=1;
+        // while(x<n){
+        //     i++;
+        //     x+=i;
+        // }
+        // // cout<<x<<" "<<n;
+        // if(x==n)    return i;
+        // return i-1;
     }
 };
