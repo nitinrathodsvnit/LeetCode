ed379a12e155b10130e105749c7ac860972ad2ef
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-07-30 06:59:51 +0530

    Time: 20 ms (72.51%), Space: 15.8 MB (91.33%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& a, char target) {
-        int n = a.size();
-        char ans='{', el = '{';
-        for(int i=0; i<n; i++){
-            if(target<a[i]) ans=min(a[i], ans);
-            el = min(a[i], el);
-        }
-        return ans=='{'?el:ans;
+        // If sorted 
+        auto it = upper_bound(a.begin(), a.end(), target);
+        return it==a.end()?a[0]:*it;
+        // If not sorted 
+        // int n = a.size();
+        // char ans='{', el = '{';
+        // for(int i=0; i<n; i++){
+        //     if(target<a[i]) ans=min(a[i], ans);
+        //     el = min(a[i], el);
+        // }
+        // return ans=='{'?el:ans;
     }
 };
