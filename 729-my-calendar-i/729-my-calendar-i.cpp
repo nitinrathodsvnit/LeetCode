ddd0c5fdb44657f772e75eb59c3615b1ff9dfb80
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-08-03 06:00:08 +0530

    Time: 161 ms (54.99%), Space: 38.8 MB (48.99%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,10 +1,13 @@
 class MyCalendar {
-    vector<pair<int, int>> books;
-public:    
+    map<int, int> mp;   // <endTime, statIme>
+public:  
     bool book(int start, int end) {
-        for (pair<int, int> p : books)
-            if (max(p.first, start) < min(end, p.second)) return false;
-        books.push_back({start, end});
-        return true;
+        /*lookfor first endTIme that is larger than start time request. First condition checkes if the incoming request should be added to the list of booked slots.*/
+        auto itr = mp.upper_bound(start);
+        if(itr == mp.end() || end<= itr->second) {
+            mp[end]=start;
+            return true;
+        } 
+        else  return false;
     }
 };
