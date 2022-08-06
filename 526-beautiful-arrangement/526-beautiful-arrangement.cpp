ed1b549b9abf8538582559d19ced9f30deba02bd
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-08-06 13:43:50 +0530

    Time: 16 ms (89.72%), Space: 6.1 MB (33.69%) - LeetHub

--- 526-beautiful-arrangement/526-beautiful-arrangement.cpp
+++ 526-beautiful-arrangement/526-beautiful-arrangement.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    int seen = 0;
-    int res = 0;
-    int countArrangement(int n, int pos = -1) {
-        if (n < 4) return n;
-        if (pos == -1) pos = n;
-        if (!pos) return res++;
-        for (int i = n, bit; i; i--) {
-            bit = 1 << i;
-            if (!(seen & bit)  && (i % pos == 0 || pos % i == 0)) {
-                // marking i as seen
-                seen ^= bit;
-                countArrangement(n, pos - 1);
-                // backtracking
-                seen ^= bit;
+    int countArrangement(int N) {
+        vector<int> vs;
+        for (int i=0; i<N; ++i) vs.push_back(i+1);
+        return counts(N, vs);
+    }
+    int counts(int n, vector<int>& vs) {
+        if (n <= 0) return 1;
+        int ans = 0;
+        for (int i=0; i<n; ++i) {
+            if (vs[i]%n==0 || n%vs[i]==0) {
+                swap(vs[i], vs[n-1]);
+                ans += counts(n-1, vs);
+                swap(vs[i], vs[n-1]);
             }
         }
-        return res;
+        return ans;
     }
 };
