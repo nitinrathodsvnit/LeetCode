680b2b2177a6469f4b0045addcb45469911e9958
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-08-20 14:39:56 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.24%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char> st;
+        for( int i=0; i<s.length(); i++){
+            if(!st.empty() and s[i]==')'){
+                if(st.top()=='(') st.pop();
+                else return false;
+            }
+            else if(!st.empty() and s[i]==']'){
+                if(st.top()=='[') st.pop();
+                else return false;
+            }
+            else if(!st.empty() and s[i]=='}'){
+                if(st.top()=='{') st.pop();
+                else return false;
+            }
+            else st.push(s[i]);
+        }
+        return st.empty();
+    }
+};
