2396b47474931cb5bff1f6e0766138e429f52a0a
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-08-29 21:04:55 +0530

    Time: 3 ms (59.72%), Space: 6.1 MB (27.01%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        return n!=0 and n!=INT_MIN? !(n&(n-1)) : 0;
+        // return n!=0 and n!=INT_MIN? !(n&(n-1)) : 0;
+        if(n==0)    return 0;
+        return ceil(log2(n))==floor(log2(n));
     }
 };
