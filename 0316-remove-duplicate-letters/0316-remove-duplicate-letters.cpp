5be20bdcc9f487a382b06544e79b6fd7eae90397
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-10-21 02:55:11 +0530

    Time: 10 ms (25.42%), Space: 6.4 MB (94.80%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        string res="";
+        int lastPos[26] = {}; 
+        bool added[26] = {}; 
+        for (int i = 0; i < s.size(); i++)
+        {
+            lastPos[s[i] - 'a'] = i;
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (added[s[i] - 'a']) continue;
+            while ( !res.empty() && res.back() > s[i] && lastPos[res.back() - 'a'] > i)
+            {
+                added[res.back() - 'a'] = false;
+                res.pop_back();
+            }
+            res.push_back(s[i]);
+            added[s[i] - 'a'] = true;
+        }
+        return res;
+    }
+};
