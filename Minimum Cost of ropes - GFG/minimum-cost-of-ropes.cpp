df21f92266dee5dba6c11cc5c564c060e86a3bb4
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2022-12-29 01:41:01 +0530

    Added solution - LeetHub

---
+++ Minimum Cost of ropes - GFG/minimum-cost-of-ropes.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long stones[], long long n) {
+        int k=2;
+       priority_queue<long long, vector<long long>, greater<long long>> heap;
+       for(int i=0; i<n; i++){
+           heap.push(stones[i]);
+       }
+        long long ans=0;
+        while(heap.size()>1){
+            if(heap.size()<k) return -1;
+            long long x=0;
+            for(int i=0; i<k; i++){
+                x+=heap.top(); heap.pop();
+            }
+            ans+=x;
+            heap.push(x);
+        }
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long i, a[n];
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        cout << ob.minCost(a, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
