63a2ee4d9ac74a91d25682cd5b87d06549548ee1
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2023-08-14 21:32:18 +0530

    Time: 1134 ms (15.22%), Space: 0B (100%) - LeetHub

--- 1258-article-views-i/1258-article-views-i.sql
+++ 1258-article-views-i/1258-article-views-i.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-SELECT DISTINCT author_id AS id FROM Views 
-WHERE author_id = viewer_id 
-ORDER BY id;
 
+# SELECT DISTINCT author_id AS id FROM Views 
+# WHERE author_id = viewer_id 
+# ORDER BY id;
+
+# GB by defalt in sorting order
+SELECT author_id AS id FROM Views 
+where author_id = viewer_id 
+GROUP BY id
+ORDER BY id;
