bb2d5835ce5b9bbd29b926642a3a9792312dd819
Nitin Rathod <89260484+nitinrathodsvnit@users.noreply.github.com>
2023-08-25 18:48:52 +0530

    Time: 8 ms (48.03%), Space: 24.8 MB (32.64%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,30 @@
+//Approach-2 (Recursion + Memoization (i, j))
+class Solution {
+public:
+    int m, n, N;
+    int t[101][101];
+    
+    bool check(string s1, string s2, string s3, int i, int j) {
+        if(i >= m && j >= n && i+j >= N) 
+            return true;
+        
+        if(i+j >= N)
+            return false;
+            
+        if(t[i][j] != -1)
+            return t[i][j];
+
+        return t[i][j] = ((s1[i]==s3[i+j] && check(s1, s2, s3, i+1, j)) || (s2[j]==s3[i+j] && check(s1, s2, s3, i, j+1)));
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        memset(t, -1, sizeof(t));
+        m = s1.length();
+        n = s2.length();
+        N = s3.length();
+        
+        if(m + n != N)
+            return false;
+        
+        return check(s1, s2, s3, 0, 0);
+    }
+};
